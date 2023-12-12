4c32c186cfe222a69b25240a89c67865724e8c02
Samuel Calistus <89456479+deracali@users.noreply.github.com>
2023-12-11 23:44:18 -0800

    Update AgeVerification.jsx

--- Frontend/src/pages/AgeVerification/AgeVerification.jsx
+++ Frontend/src/pages/AgeVerification/AgeVerification.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './AgeVerification.css'
 import { Helmet } from 'react-helmet-async'
+import {Link} from "react-router-dom"
 
 export default function AgeVerification() {
   return (
@@ -12,8 +13,8 @@ export default function AgeVerification() {
     <span className="age-title">Are you 18 or older?</span>
     <span className="age-main-text">You must be of legal age to view our website. Due to legal requirements you must verify your age.</span>
     
-    <a href='/index' className="age-button age-yes">I am over 18</a>
-    <a href='' className="age-button age-no">Exit</a>
+    <Link href='/index' className="age-button age-yes">I am over 18</Link>
+    <Link href='' className="age-button age-no">Exit</Link>
   </div>
 </div>
   )
